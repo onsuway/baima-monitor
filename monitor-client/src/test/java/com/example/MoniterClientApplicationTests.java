78656adce181084b24ee5108c8e0beb5b0d6e960
susuya <1016620694@qq.com>
2024-02-01 14:19:45 +0800

    Revert "docs: 修改application.yml隐私、增加SQL文件"
    This reverts commit 00464dc2e2a3661b262b1332bbc03b283a758081.

---
+++ monitor-client/src/test/java/com/example/MoniterClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootTest
+class MoniterClientApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("123456"));
+    }
+
+}
